4341a82ca2c5e9edb225f8d5fd61b6addfa11f10
Augusto Yuudi Shiraishi <augustoyuudi@gmail.com>
2021-07-22 23:51:47 -0300

    feat: start structure to play with framermotion

---
+++ src/pages/Brand.tsx
@@ -0,0 +1,7 @@
+export function Brand() {
+  return (
+    <main>
+      <h1>ola brand</h1>
+    </main>
+  )
+}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import tailwind from '../assets/tailwindcss.svg';
+
+export function Home() {
+  return (
+    <main>
+      <img
+        src={tailwind}
+        alt="Tailwind CSS brand logo"
+      />
+    </main>
+  )
+}
